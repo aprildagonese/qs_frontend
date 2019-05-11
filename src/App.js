d74019e4f4435680db1b62d3321b5b8b05ead80c
April Dagonese <adagonese@gmail.com>
2019-05-11 13:23:28 -0600

    Troubleshooting fetch call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import axios from 'axios';
+// import pry from 'pryjs';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      current_ingredient: null,
+      recipe_data: null,
+      calorie_data: null,
+      isLoading: false,
+      error: null,
+      testing: "apples"
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    const url = "https://choosin-foods-recipes.herokuapp.com/api/v1/recipes?key=9c18d10fb5a0f5a8aa140aa5e94ceb87&ingredient=Chicken"
+    fetch(url)
+      .then(result => {
+        console.log("BUTTS")
+        console.log(result.data)
+      })
+      .then(result => this.setState({
+        current_ingredient: "Salmon",
+        recipe_data: result,
+        isLoading: false
+      }))
+      .catch(error => this.setState({
+        error,
+        isLoading: false
+      }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Here is the recipe data: {this.state.current_ingredient}
+      </div>
+    )
+  }
+};
 
 export default App;
