1b076e99fdd373daf1ec62f9298927d3554c5035
April Dagonese <adagonese@gmail.com>
2019-05-12 14:34:24 -0600

    Deletes commented notes

--- src/Calendar.js
+++ src/Calendar.js
@@ -25,23 +25,5 @@ class Calendar extends Component {
     );
   }
 }
-//   render(dates) {
-//     return (
-//       <div className='calendar'>
-//         {this.props.dates.meals.dates.first}
-//         {Object.keys(this.props.dates.meals.dates).map(date => {
-//           const newDate = new Date(date)
-//           const prettyDate = newDate.toLocaleDateString('en-US')
-//           return <CalDate key={prettyDate}
-//                           date={prettyDate}
-//                           dateMeals={date}
-//                           calSetDate={this.props.setDate}
-//                  />
-//           })
-//         }
-//       </div>
-//     );
-//   }
-// }
 
 export default Calendar;
