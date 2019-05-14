c3478ed3f4a817c216cadf9d06571e2e1a3e6667
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-14 16:41:49 -0600

    Adds conditional to render Food

--- src/FoodShow.js
+++ src/FoodShow.js
@@ -11,16 +11,20 @@ class FoodShow extends Component {
   render() {
     return(
       <div>
-        <h3>{this.props.food.name}</h3>
-        <h4>Calories: {this.props.food.calories}</h4>
-        <h4>Recipes</h4>
-        {this.props.recipes && this.props.recipes.recipes.map(recipe => {
-          return <a onClick={() =>  window.open(recipe.recipe_url, "_blank")}
-                    href={recipe.url}
-                    target="_blank">
-            {recipe.label}<br/>
-          </a>
-        })}
+      { this.props.food &&
+        <>
+          <h3>{this.props.food.name}</h3>
+          <h4>Calories: {this.props.food.calories}</h4>
+          <h4>Recipes</h4>
+          {this.props.recipes && this.props.recipes.recipes.map(recipe => {
+            return <a onClick={() =>  window.open(recipe.recipe_url, "_blank")}
+                      href={recipe.url}
+                      target="_blank">
+              {recipe.label}<br/>
+            </a>
+          })}
+        </>
+      }
       </div>
     )
   }
