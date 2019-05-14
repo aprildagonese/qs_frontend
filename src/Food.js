aa71b1ff25d36d1e9f2c65622263e26171080790
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-13 21:06:19 -0600

    Adds Modal to Food

--- src/Food.js
+++ src/Food.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Modal from './Modal';
 import { deleteFood } from './services/calories'
 
 class Food extends Component {
   constructor() {
     super()
+    this.state = {
+      showModal: false
+    }
   }
 
   deleteRecord = () => {
@@ -14,14 +18,31 @@ class Food extends Component {
     console.log(userID, mealID, foodID);
   }
 
+  showModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  hideModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  showFood = () => {
+    this.showModal()
+  }
+
   render() {
     return(
     <div>
-      <li>{this.props.data.name}, {this.props.data.calories} calories</li>
+      <li>
+      <a onClick={this.showFood}>{this.props.data.name}</a>, {this.props.data.calories} calories
       <button className='delete-food'
-              onClick={this.deleteRecord}>
-              X
+      onClick={this.deleteRecord}>
+      X
       </button>
+      </li>
+      <Modal hideModal={this.hideModal}
+             showModal={this.state.showModal}
+             type="foodShow"/>
     </div>
   )}
 }
