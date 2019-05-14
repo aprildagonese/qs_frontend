f8f52e5e170cce17341a15586645ad8b2bafefaa
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-13 21:07:37 -0600

    Creates FoodShow comp

---
+++ src/FoodShow.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class FoodShow extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <div>
+        Food Show
+      </div>
+    )
+  }
+}
+
+export default FoodShow;
