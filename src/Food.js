2941a03019070ce6ee431436a95e4034370f6af1
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-13 19:51:36 -0600

    Adds update to Food comp

--- src/Food.js
+++ src/Food.js
@@ -6,10 +6,22 @@ class Food extends Component {
     super()
   }
 
+  deleteRecord = () => {
+    const userID = this.props.userID;
+    const mealID = this.props.mealID;
+    const foodID = this.props.data.id;
+    // deleteFood(userID, mealID, foodID);
+    console.log(userID, mealID, foodID);
+  }
+
   render() {
     return(
     <div>
       <li>{this.props.data.name}, {this.props.data.calories} calories</li>
+      <button className='delete-food'
+              onClick={this.deleteRecord}>
+              X
+      </button>
     </div>
   )}
 }
